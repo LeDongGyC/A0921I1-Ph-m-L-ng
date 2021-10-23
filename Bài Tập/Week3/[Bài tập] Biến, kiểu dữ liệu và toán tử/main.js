3c87bc24d47d54df4fc15cd3ebfcf7e13c8b285b
LeDongGyC <dongleepham@gmail.com>
2021-10-23 23:38:42 +0700

    update BT&TH Week3(CâuÐK)23/10/2021

---
+++ Bài Tập/Week3/[Bài tập] Biến, kiểu dữ liệu và toán tử/main.js
@@ -0,0 +1,38 @@
+// Bài 1:
+// let diemLy
+// let diemHoa
+// let diemSinh
+// diemLy = prompt("Nhập điểm Vật Lý của bạn:")
+// diemHoa = prompt("Nhập điểm Hoá học  của bạn:")
+// diemSinh = prompt("Nhập điểm Sinh học của bạn:")
+// let a = parseInt(diemLy)
+// let b = parseInt(diemHoa)
+// let c = parseInt(diemSinh)
+// let Tg=a+b+c
+// document.write('Tổng 3 môn:'+Tg)
+// let TBC=(a+b+c)/3
+// document.write('</br>')
+// document.write('Trung bình cộng 3 môn:'+TBC)
+
+// Bài 2:
+//  let Celsius
+//  Celsius=prompt("Enter the Celsius")
+//  a=parseInt(Celsius)
+//  let b=(9*a)/5+32
+//  document.write("Độ F:"+b)
+
+// Bài 3:
+// let Bankinh
+// Bankinh=prompt("Nhập bán kính của hình tròn:")
+// r=parseInt(Bankinh)
+// const pi=3.14
+// let DT=r*r*pi
+// document.write("Diện tích hình tròn là:"+DT)
+
+// Bài 4
+// let Bankinh
+// Bankinh=prompt("Nhập bán kính của hình tròn:")
+// r=parseInt(Bankinh)
+// const pi=3.14
+// let DT=2*r*pi
+// document.write("Diện tích hình tròn là:"+DT)
